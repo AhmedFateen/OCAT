eee7b5032d447291ef60614adc654845d4974cce
AhmedFateen <59644874+AhmedFateen@users.noreply.github.com>
2021-07-26 15:28:44 -0400

    Update index.js

--- OCAT/server/libs/AssessmentService/index.js
+++ OCAT/server/libs/AssessmentService/index.js
@@ -4,7 +4,7 @@ const { InternalServerError } = require(`restify-errors`);
 exports.submit = (assessment) => new Promise((resolve, reject) => {
   // this function sends a request to the API
   // finish the logic to handle the response when returned from the API
-  client.METHOD(`/some-url`,
+  client.post(`/submit`,
     (err, req, res, body) => {
       if (err) {
         return reject(err);
@@ -13,8 +13,8 @@ exports.submit = (assessment) => new Promise((resolve, reject) => {
       if (res.statusCode !== 200) {
         return reject(new InternalServerError(`Request Error`));
       }
-
       resolve(body.data);
+      return assessment;
     });
 });
 
