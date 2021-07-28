ebb139a60795d9b893c0d136398da01243281622
AhmedFateen <59644874+AhmedFateen@users.noreply.github.com>
2021-07-28 15:26:21 -0400

    mainly Assessment-Service index.js

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -3,6 +3,21 @@ const { Assessments } = require(`../Database`);
 exports.submit = async (assessment) => {
   // use the bookshelf model Assessments from API/src/microservices/Database to save
   // the assessment data in the PostgreSQL database
+  assessment.created_at = new Date();
+  assessment.score = parseInt(assessment.PreviousContact) + parseInt(assessment.PhysicalAltercationsWithOtherCats) +
+  parseInt(assessment.PhysicalAltercationsWithOwner) + parseInt(assessment.PlaysWellWithDogs) +
+  parseInt(assessment.HissesAtStrangers);
+
+  assessment.riskLevel = assessment.score > 3 ? `high` :
+    assessment.score > 1 ? `medium` :
+      `low`;
+  Assessments.forge({
+    score: assessment.score,
+    risk_level: assessment.riskLevel,
+    cat_name: assessment.CatName,
+    cat_date_of_birth: assessment.CatDateOfBirth,
+    created_at: assessment.created_at,
+  }).save();
 };
 
 exports.getList = () => {
--- OCAT/client/pages/Assessments/NewAssessment.jsx
+++ OCAT/client/pages/Assessments/NewAssessment.jsx
@@ -12,46 +12,55 @@ export const NewAssessment = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="instrument">Instrument:&nbsp;</label>
-      <input type="text" id="instrument" readonly="readonly" />
+      <input type="text" id="instrument" readOnly="readOnly" />
       <br />
       <label htmlFor="catName">Cat Name:&nbsp;</label>
-      <input type="text" id="catName" placeholder="Cat Name" {...register(`Cat Name`, { required: true })} />
+      <input type="text" name="catName" id="catName" placeholder="Cat Name" {...register(`CatName`,
+        { required: true })} />
       <br />
       <label htmlFor="dateOfBirth">Cat Date of Birth:&nbsp;</label>
-      <input type="date" id="dateOfBirth" {...register(`Cat Date of Birth`, { required: true })} />
+      <input type="date" id="dateOfBirth" {...register(`CatDateOfBirth`, { required: true })} />
       <br />
       <label htmlFor="previousContact">Previous Contact with the Cat Judicial System:&nbsp;</label>
       <label htmlFor="noPreviousContact">No&nbsp;</label>
-      <input {...register(`Previous Contact with the Cat Judicial\
-       System`, { required: true })} type="radio" value="0" id="noPreviousContact" />
+      <input {...register(`PreviousContact`,
+        { required: true })} type="radio" value="0" id="noPreviousContact" />
       <label htmlFor="yesPreviousContact">Yes&nbsp;</label>
-      <input {...register(`Previous Contact with the Cat Judicial\
-       System`, { required: true })} type="radio" value="1" id="yesPreviousContact" />
+      <input {...register(`PreviousContact`,
+        { required: true })} type="radio" value="1" id="yesPreviousContact" />
 
       <br />
       <label htmlFor="altercationsWithCats">Physical altercations with other cats:&nbsp;</label>
       <label htmlFor="0-3 altercations">0-3 altercations&nbsp;</label>
-      <input {...register(`Physical altercations with other cats`, { required: true })} type="radio" value="0" />
+      <input {...register(`PhysicalAltercationsWithOtherCats`,
+        { required: true })} type="radio" value="0" />
       <label htmlFor="3+ altercations">3+ altercations&nbsp;</label>
-      <input {...register(`Physical altercations with other cats`, { required: true })} type="radio" value="1" />
+      <input {...register(`PhysicalAltercationsWithOtherCats`,
+        { required: true })} type="radio" value="1" />
       <br />
       <label htmlFor="altercationsWithOwner">Physical altercations with owner (scratching, biting, etc...): </label>
       <label htmlFor="0-10 altercations">0-10 altercations&nbsp;</label>
-      <input {...register(`Physical altercations with owner`, { required: true })} type="radio" value="0" />
+      <input {...register(`PhysicalAltercationsWithOwner`,
+        { required: true })} type="radio" value="0" />
       <label htmlFor="10+ altercations">10+ altercations&nbsp;</label>
-      <input {...register(`Physical altercations with owner`, { required: true })} type="radio" value="1" />
+      <input {...register(`PhysicalAltercationsWithOwner`,
+        { required: true })} type="radio" value="1" />
       <br />
       <label htmlFor="goodWithDogs">Plays well with dogs:&nbsp;</label>
       <label htmlFor="noDogs">No&nbsp;</label>
-      <input {...register(`Plays well with dogs`, { required: true })} type="radio" value="1" id="noDogs" />
+      <input {...register(`PlaysWellWithDogs`,
+        { required: true })} type="radio" value="1" id="noDogs" />
       <label htmlFor="yesDogs">Yes&nbsp;</label>
-      <input {...register(`Plays well with dogs`, { required: true })} type="radio" value="0" id="yesDogs" />
+      <input {...register(`PlaysWellWithDogs`,
+        { required: true })} type="radio" value="0" id="yesDogs" />
       <br />
       <label htmlFor="hisses">Hisses at strangers:&nbsp;</label>
       <label htmlFor="noHisses">No&nbsp;</label>
-      <input {...register(`Hisses at strangers`, { required: true })} type="radio" value="0" id="noHisses" />
+      <input {...register(`HissesAtStrangers`,
+        { required: true })} type="radio" value="0" id="noHisses" />
       <label htmlFor="yesHisses">Yes&nbsp;</label>
-      <input {...register(`Hisses at strangers`, { required: true })} type="radio" value="1" id="yesHisses" />
+      <input {...register(`HissesAtStrangers`,
+        { required: true })} type="radio" value="1" id="yesHisses" />
       <br />
       <Button variant="primary" type="submit">Submit</Button>
 
